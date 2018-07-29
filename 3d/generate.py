a72a477e2bc5ed0388cc48f85b9c9981dcafb8af
Danya Generalov <danya02@me.com>
2018-07-29 21:14:25 +0300

    Adding parallelization to generator.

--- 3d/generate.py
+++ 3d/generate.py
@@ -1,3 +1,4 @@
+import multiprocessing
 def test(a,b,c,d,l):
     asq=a**2
     bsq=b**2
@@ -6,27 +7,36 @@ def test(a,b,c,d,l):
     lsq=l**2
     return 3*((l**4)+(b**4)+(a**4)+(c**4)+(d**4)) == 2*(bsq*lsq+asq*lsq+asq*bsq+bsq*csq+csq*lsq+asq*csq+asq*dsq+bsq*dsq+csq*dsq+lsq*dsq)
 
-limit=150
-found = []
-try:
-    with open('steinhaus-3d-tetrahedron') as o:
-        for i in o:
-            i=i.strip()
-            if i:
-                found.append(sorted([int(n) for n in i.split(' ')]))
-except FileNotFoundError:
-    pass
-
-for a in range(1,limit):
-    for b in range(1,limit):
-        for c in range(1,limit):
-            for d in range(1,limit):
-                for l in range(1,limit):
-                    print(f'a={a} b={b} c={c} d={d} l={l}')
-                    if sorted([a,b,c,d,l]) not in found:
+limit=60
+procs = 16
+def worker(num):
+    print('I am worker',num)
+    for a in range(num,limit,procs):
+        for b in range(1,limit):
+            print(f'{num}: a={a}, b={b}')
+            for c in range(1,limit):
+                for d in range(1,limit):
+                    for l in range(1,limit):
                         if test(a,b,c,d,l):
-                            with open('steinhaus-3d-tetrahedron','a') as o:
+                            with open(f'steinhaus-3d-tetrahedron.part{num}','a') as o:
                                 print(a,b,c,d,l,file=o)
-                                print(a,b,c,d,l)
-                                found.append(sorted([a,b,c,d,l]))
+                                print(num,': FOUND!!!!!',a,b,c,d,l)
 
+proclist = [multiprocessing.Process(target=worker,args=tuple([i+1])) for i in range(procs)]
+for i in proclist:
+    i.start()
+try:
+    for i,j in enumerate(proclist):
+        print(f'Waiting for process {i}...')
+        j.join()
+finally:
+    print('Combining data!')
+    data=[]
+    for i in range(procs):
+        try:
+            data.extend(open(f'steinhaus-3d-tetrahedron.part{i+1}'))
+        except:
+            pass
+    data.extend(open('steinhaus-3d-tetrahedron'))
+    data=set(data)
+    open('steinhaus-3d-tetrahedron','w').write(''.join(data))
